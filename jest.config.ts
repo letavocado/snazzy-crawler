2452bd2fd013658c03156891c80c04d487e00665
Amirzhan Aliyev <letavocado@proton.me>
2023-06-08 00:47:52 +0600

    chore: update configs

--- jest.config.ts
+++ jest.config.ts
@@ -1,23 +1,25 @@
-import type { JestConfigWithTsJest } from 'ts-jest'
+import type { JestConfigWithTsJest } from "ts-jest";
 
 const jestConfig: JestConfigWithTsJest = {
-  roots: ['<rootDir>'],
-
-  preset: 'ts-jest/presets/default-esm', // or other ESM presets
+  preset: "ts-jest/presets/default-esm", // or other ESM presets
+  moduleFileExtensions: ["ts", "js"],
   moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1'
+    "^(\\.{1,2}/.*)\\.js$": "$1",
   },
   transform: {
     // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
     // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
-    '^.+\\.tsx?$': [
-      'ts-jest',
+    "^.+\\.tsx?$": [
+      "ts-jest",
       {
-        useESM: true
-      }
-    ]
+        useESM: true,
+        tsconfig: "tsconfig.json",
+      },
+    ],
   },
-  transformIgnorePatterns: ['node_modules/']
-}
+  testMatch: ["**/test/**/*.test.(ts|js)"],
+  transformIgnorePatterns: ["node_modules/"],
+  testEnvironment: "node",
+};
 
-export default jestConfig
+export default jestConfig;
